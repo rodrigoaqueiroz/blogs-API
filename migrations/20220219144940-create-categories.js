65f2b23e7ea8ccd3c41cd5efb39a4efbeb17b4ea
rodrigoaqueiroz <rodrigoandradequeiroz@gmail.com>
2022-02-19 12:09:26 -0300

    feat: create-categories table

---
+++ migrations/20220219144940-create-categories.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Categories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Categories')
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
